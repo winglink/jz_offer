ec8cecb0cd8c384d8b3b95a4b5161942196d2140
ww <zyq00123@icloud.com>
2019-09-05 17:02:33 +0800

    timu3

--- pro_1.java
+++ pro_1.java
@@ -1,2 +1,27 @@
 public class pro_1 {
+    public static   void main(String[] args){
+        /******** 题3 数组中的重复数字********/
+       // int[] a={1,2,3,4,5,6,2};
+      //  int[] a={0,1,2,3,4};
+        int[] a={3,3,3,3,3};
+        int result=-1;
+        for(int i=0; i < a.length;){
+            System.out.println(a[i]);
+            if(a[i] == i) {
+                i++;
+                continue;
+            }
+            else {
+                if(a[i] == a[a[i]]){
+                   result=a[i];
+                   System.out.println("result="+result);
+                   return;
+                }
+                //swap
+               int temp=a[a[i]];
+                a[a[i]]=a[i];
+                a[i]=temp;
+            }
+        }
+    }
 }
