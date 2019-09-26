6a8d4a8b4251068fb52415e2b21d2f7b2555002d
ww <zyq00123@icloud.com>
2019-09-26 21:38:46 +0800

    complete 7

--- pro_1.java
+++ pro_1.java
@@ -1,19 +1,35 @@
+import javax.swing.text.html.HTMLDocument;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class pro_1 {
     public static   void main(String[] args){
-         int[] a={1,2,5,3,4,5};
-         int[][] aa={
-                 {1,2,8,9},
-                 {2,3,9,12},
-                 {4,7,10,13},
-                 {6,8,11,15},
-         };
-           String ss="we are happy";
-           char[] cc=ss.toCharArray();
-           System.out.println(cc);
-           System.out.println(cc.length);
-           cc=th_space(cc);
-        System.out.println(cc);
+//         int[] a={1,2,5,3,4,5};
+//         int[][] aa={
+//                 {1,2,8,9},
+//                 {2,3,9,12},
+//                 {4,7,10,13},
+//                 {6,8,11,15},
+//         };
+//           String ss="we are happy";
+//           char[] cc=ss.toCharArray();
+//           System.out.println(cc);
+//           System.out.println(cc.length);
+//           cc=th_space(cc);
+//        System.out.println(cc);
 //        System.out.println(double_in(aa,6,0,aa[0].length-1));
+/*****************7重建二叉树*************************/
+        int[] a={1,2,4,7,3,5,6,8};
+        int[] b={4,7,2,1,5,3,8,6};
+        Tree tree=new Tree();
+        tree.root=cj_Tree(a,b,tree);
+        System.out.println(tree.root);
+        tree.zx_print();
+
+
+
+/*****************7重建二叉树*************************/
     }
     /**********题目3 数组中的重复数字 *****************/
     public void find_c(int[] a ) {
@@ -98,4 +114,90 @@ public class pro_1 {
             }
             return xin;
     }
+      /**************7:重建二叉树*****************/
+     public static TreeNode cj_Tree(int[] qx,int[] zx,Tree tree){
+         if(qx.length == 0 || zx.length == 0)
+             return null;
+
+         int gen=qx[0];
+         TreeNode treeNode=new TreeNode(gen);
+         for(int i=0;i<qx.length-1;i++)
+         {
+             qx[i]=qx[i+1];
+         }
+         int i=0;
+         for(;i<zx.length;i++)
+         {
+             if(zx[i]==gen)
+                 break;
+         }
+         int lengtha=0;
+         if( i == 0)
+         {
+             lengtha=0;
+         }else {
+             lengtha=i;
+         }
+         System.out.println("i="+i);
+         System.out.println("lengtha="+lengtha);
+         System.out.println("zx.length="+zx.length);
+         System.out.println(Arrays.toString(zx));
+         int[] leftzx=new int[lengtha];
+         for (int j = 0; j < i ; j++) {
+             leftzx[j] = zx[j];
+         }
+         int lengthb=0;
+         if(i == zx.length-1)
+         {
+             lengthb=0;
+         }else {
+             lengthb=zx.length-1-i;
+         }
+         System.out.println("i="+i);
+         System.out.println("lengthb="+lengthb);
+         System.out.println("zx.length="+zx.length);
+         System.out.println(Arrays.toString(zx));
+
+         int[] rightzx=new int[lengthb];
+         for(int j=0;j < lengthb;j++)
+         {
+             rightzx[j]=zx[i+1+j];
+         }
+         treeNode.leftchild=cj_Tree(qx,leftzx,tree);
+         treeNode.rightchild=cj_Tree(qx,rightzx,tree);
+         return  treeNode;
+
+
+     }
 }
+class  TreeNode{
+       int  key;
+       TreeNode leftchild;
+       TreeNode rightchild;
+       TreeNode(int key){
+           this.key=key;
+           leftchild=null;
+           rightchild=null;
+       }
+       @Override
+      public  String toString(){
+           return  "key="+key;
+       }
+}
+class Tree{
+     TreeNode root;
+     Tree(){
+         root=null;
+     }
+     public  void  zx_print(){
+          print(root);
+     }
+     public  void print(TreeNode node){
+         if(node == null)
+             return;
+         print(node.leftchild);
+          System.out.print(node);
+          print(node.rightchild);
+     }
+}
+
